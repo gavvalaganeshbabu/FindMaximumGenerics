7c0edda7cc3cc3d988d2ab533ec1b9e7d8a9bbfb
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-02-15 07:56:43 +0530

    Add project files.

---
+++ FindMaximumGenerics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FindMaximumGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome TO TestMaximum Application By Using Generics :");
+            Console.WriteLine("Enter the Values What you want to check :");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int output = FindMaximum.FindMaximumValue(a, b, c);
+            Console.WriteLine("Maximum Value = " + output);
+        }
+    }
+}
