e7c6e04a182724b7f54545ab2919a7ca3dccdbc3
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-02-15 09:20:38 +0530

    Added Code

--- FindMaximumGenerics/Program.cs
+++ FindMaximumGenerics/Program.cs
@@ -6,11 +6,11 @@ namespace FindMaximumGenerics
     {
         static void Main(string[] args)
         {
-            int output = FindMaximum<int>.GetMaximum(15, 25, 5);
+            int output = FindMaximum<int>.GetMaximum(215, 7825, 2725);
             Console.WriteLine($"max={output}");
-            float output1 = FindMaximum<float>.GetMaximum(15.5f, 253.5f, 3.5f);
+            float output1 = FindMaximum<float>.GetMaximum(34.5f, 253.5f, 334.5f);
             Console.WriteLine($"max={output1}");
-            string output2 = FindMaximum<string>.GetMaximum("abc", "pqr", "lmn");
+            string output2 = FindMaximum<string>.GetMaximum("Ganesh", "Gavvala", "The");
             Console.WriteLine($"max={output2}");
 
         }
