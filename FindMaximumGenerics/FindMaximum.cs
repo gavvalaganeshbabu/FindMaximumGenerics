7b6362c8854ef6838fa2f355432462d0196759fc
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-02-15 09:12:15 +0530

    Added GetGeneric method Code

--- FindMaximumGenerics/FindMaximum.cs
+++ FindMaximumGenerics/FindMaximum.cs
@@ -4,39 +4,28 @@ using System.Text;
 
 namespace FindMaximumGenerics
 {
- 
-        public class FindMaximum<T> where T : IComparable<T>
+    public class FindMaximum
+    {
+        public static T GetMaximum<T>(T first_Value, T second_Value, T third_Value) where T : IComparable<T>
         {
-            private T first_Value;
-            private T second_Value;
-            private T third_Value;
-            public FindMaximum(T first_Value, T second_Value, T third_Value)
+            if (first_Value.CompareTo(second_Value) > 0 && first_Value.CompareTo(third_Value) > 0)
             {
-                this.first_Value = first_Value;
-                this.second_Value = second_Value;
-                this.third_Value = third_Value;
+                return first_Value;
             }
-            public static T GetMaximum(T first_Value, T second_Value, T third_Value)
+            else if (second_Value.CompareTo(first_Value) > 0 && second_Value.CompareTo(third_Value) > 0)
             {
-                if (first_Value.CompareTo(second_Value) > 0 && first_Value.CompareTo(third_Value) > 0)
-                {
-                    return first_Value;
-                }
-                else if (second_Value.CompareTo(first_Value) > 0 && second_Value.CompareTo(third_Value) > 0)
-                {
-                    return second_Value;
-                }
-                else if (third_Value.CompareTo(first_Value) > 0 && third_Value.CompareTo(second_Value) > 0)
-                {
-                    return third_Value;
-                }
-                else
-                {
-                    throw new Exception("Values are same");
-                }
-
+                return second_Value;
+            }
+            else if (third_Value.CompareTo(first_Value) > 0 && third_Value.CompareTo(second_Value) > 0)
+            {
+                return third_Value;
+            }
+            else
+            {
+                throw new Exception("Values are The same");
             }
         }
     }
+}
 
 
--- FindMaximumGenerics/Program.cs
+++ FindMaximumGenerics/Program.cs
@@ -6,22 +6,12 @@ namespace FindMaximumGenerics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome TO TestMaximum Application By Using Generics :");
-<<<<<<< HEAD
-            Console.WriteLine("Enter the String values What you want to check :");
-            string a = Console.ReadLine();
-            string b = Console.ReadLine();
-            string c = Console.ReadLine();
-            string output = FindMaximum.MaxString(a, b, c);
-            Console.WriteLine("Maximum Value = " + output);
-=======
-            int output = FindMaximum<int>.GetMaximum(29, 256, 598);
+            int output = FindMaximum.GetMaximum<int>(456, 254, 385);
             Console.WriteLine($"Maximum= {output}");
-            float output1 = FindMaximum<float>.GetMaximum(153.5f, 233.5f, 455.5f);
+            float output1 = FindMaximum.GetMaximum<float>(135.5f, 335.5f, 275.5f);
             Console.WriteLine($"Maximum= {output1}");
-            string output2 = FindMaximum<string>.GetMaximum("Ganesh", "Gavvala", "The Great");
+            string output2 = FindMaximum.GetMaximum<string>("Ganesh", "Gavvala", "The");
             Console.WriteLine($"Maximum= {output2}");
->>>>>>> UC4
         }
     }
 }
