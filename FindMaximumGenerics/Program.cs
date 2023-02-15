57dfa0661d85d6e41c3af9e0cc9a46ac71c0bf73
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-02-15 09:17:11 +0530

    Addded Code

--- FindMaximumGenerics/Program.cs
+++ FindMaximumGenerics/Program.cs
@@ -6,14 +6,12 @@ namespace FindMaximumGenerics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome TO TestMaximum Application By Using Generics :");
-            Console.WriteLine("Enter the String values What you want to check :");
-            int output = FindMaximum<int>.GetMaximum(29, 256, 598);
-            Console.WriteLine($"Maximum= {output}");
-            float output1 = FindMaximum<float>.GetMaximum(153.5f, 233.5f, 455.5f);
-            Console.WriteLine($"Maximum= {output1}");
-            string output2 = FindMaximum<string>.GetMaximum("Ganesh", "Gavvala", "The Great");
-            Console.WriteLine($"Maximum= {output2}");
+            int output = FindMaximum<int>.GetMaximum(15, 25, 5);
+            Console.WriteLine($"max={output}");
+            float output1 = FindMaximum<float>.GetMaximum(15.5f, 253.5f, 3.5f);
+            Console.WriteLine($"max={output1}");
+            string output2 = FindMaximum<string>.GetMaximum("abc", "pqr", "lmn");
+            Console.WriteLine($"max={output2}");
 
         }
     }
